72af1c1abf7a5243c5c76c982e13f4c3ae0952e2
github-cloudlabsuser-994 <167555227+github-cloudlabsuser-994@users.noreply.github.com>
2024-05-05 17:15:06 +0000

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -35,46 +35,46 @@ def index():
 # Code was taken from PyGoat: https://github.com/adeyosemanputra/pygoat/blob/master/introduction/views.py#L137
 # Modified for it to be called once a user hits the endpoint and provides data
 # @flaskapp.route('/sql-example')
-# def sql_lab(request):
-#     if request.user.is_authenticated:
+def sql_lab(request):
+     if request.user.is_authenticated:
 
-#         name=request.POST.get('name')
+         name=request.POST.get('name')
 
-#         password=request.POST.get('pass')
+         password=request.POST.get('pass')
 
-#         if name:
+         if name:
 
-#             if login.objects.filter(user=name):
+             if login.objects.filter(user=name):
 
-#                 sql_query = "SELECT * FROM introduction_login WHERE user='"+name+"'AND password='"+password+"'"
-#                 print(sql_query)
-#                 try:
-#                     print("\nin try\n")
-#                     val=login.objects.raw(sql_query)
-#                 except:
-#                     print("\nin except\n")
-#                     return render(
-#                         request, 
-#                         'Lab/SQL/sql_lab.html',
-#                         {
-#                             "wrongpass":password,
-#                             "sql_error":sql_query
-#                         })
+                 sql_query = "SELECT * FROM introduction_login WHERE user='"+name+"'AND password='"+password+"'"
+                 print(sql_query)
+                 try:
+                     print("\nin try\n")
+                     val=login.objects.raw(sql_query)
+                 except:
+                     print("\nin except\n")
+                     return render(
+                         request, 
+                         'Lab/SQL/sql_lab.html',
+                         {
+                             "wrongpass":password,
+                             "sql_error":sql_query
+                         })
 
-#                 if val:
-#                     user=val[0].user
-#                     return render(request, 'Lab/SQL/sql_lab.html',{"user1":user})
-#                 else:
-#                     return render(
-#                         request, 
-#                         'Lab/SQL/sql_lab.html',
-#                         {
-#                             "wrongpass":password,
-#                             "sql_error":sql_query
-#                         })
-#             else:
-#                 return render(request, 'Lab/SQL/sql_lab.html',{"no": "User not found"})
-#         else:
-#             return render(request, 'Lab/SQL/sql_lab.html')
-#     else:
-#         return redirect('login')
+                 if val:
+                     user=val[0].user
+                     return render(request, 'Lab/SQL/sql_lab.html',{"user1":user})
+                 else:
+                     return render(
+                         request, 
+                         'Lab/SQL/sql_lab.html',
+                         {
+                             "wrongpass":password,
+                             "sql_error":sql_query
+                         })
+             else:
+                 return render(request, 'Lab/SQL/sql_lab.html',{"no": "User not found"})
+         else:
+             return render(request, 'Lab/SQL/sql_lab.html')
+     else:
+         return redirect('login')
